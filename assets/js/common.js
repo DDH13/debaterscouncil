6db137e2e4465a2b0044664b3b8f061461bb27f0
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-07-19 11:39:22 +0530

    Fixed contact info bug

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,16 +1,58 @@
-    const loadFooter = () => {
-        const footer = document.querySelector("footer");
-        fetch("./templates/footer.html")
-            .then(response => response.text())
-            .then(data => {
-                footer.innerHTML = data;
+const loadFooter = () => {
+    const footer = document.querySelector("footer");
+    fetch("./templates/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            footer.innerHTML = data;
+        });
+}
+const loadHeader = () => {
+    const header = document.querySelector("header");
+    fetch("./templates/header.html")
+        .then(response => response.text())
+        .then(data => {
+            header.innerHTML = data;
+        });
+}
+const loadHeaderFooter = () => {
+    const header = document.querySelector("header");
+    const footer = document.querySelector("footer");
+    fetch("./templates/header.html")
+        .then(response => response.text())
+        .then(data => {
+            header.innerHTML = data;
+        })
+        .then(() => {
+            fetch("./templates/footer.html")
+                .then(response => response.text())
+                .then(data => {
+                    footer.innerHTML = data;
+                });
+        })
+        .then(() => {
+            attachScroll();
+        })
+}
+
+function attachScroll() {
+    const contactButton = document.querySelector(".cta-contact");
+    contactButton.addEventListener("click", function () {
+        // Scroll to the footer
+        document.querySelector(".page-footer").scrollIntoView({ behavior: "smooth" });
+
+        // Add a delay before adding the class to the icons for a smoother scrolling effect
+        setTimeout(function () {
+            const icons = document.querySelectorAll(".page-footer i");
+            icons.forEach(icon => {
+                icon.classList.add("scale-effect");
+                // Remove the "scale-effect" class after a short delay (e.g., 4 seconds for both scale up and down)
+                setTimeout(() => icon.classList.remove("scale-effect"), 4000);
             });
-    }
-    const loadHeader = () => {
-        const header = document.querySelector("header");
-        fetch("./templates/header.html")
-            .then(response => response.text())
-            .then(data => {
-                header.innerHTML = data;
-            });
-    }
+        }, 500); // Adjust the delay (in milliseconds) to your preference
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadHeaderFooter();
+});
