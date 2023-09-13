4c5dedb8abdaec34f19e2d6cf92f3a10e39de04a
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-09-13 14:39:45 +0530

    added ASCII art and message

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    console.log('%cHi, if you are seeing this then you are poking about the website, if you are interested in helping me (DDH) make the website better drop me an email at debaterscouncil@gmail.com', 'font-size: 36px; font-weight: bold; color: #ed212e;');
 
 });
